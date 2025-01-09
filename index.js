62ac31be86d32cef4293a842cab13c45bbe8760d
SaikatAhmed78 <shaikatahmed78@gmail.com>
2025-01-09 23:47:52 +0600

    Save payment info and delete cart items in the database

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SK)
+const jwt = require('jsonwebtoken');
+
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://bistro-boss-restaurant-362e2.web.app',
+    'https://bistro-boss-restaurant-362e2.firebaseapp.com'
+  ]
+}
+));
+
 app.use(express.json());
 
 
@@ -23,28 +34,243 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     await client.connect();
 
-        // Database Collection
+    // Database Collection
+    const usersCollection = client.db("bistro-boss-DB").collection("users");
     const menuCollection = client.db("bistro-boss-DB").collection("menu");
-    const reviewCollection = client.db("bistro-boss-DB").collection("reviews");
+    const cartCollection = client.db("bistro-boss-DB").collection("carts");
+    const reviewCollection = client.db("bistro-boss-DB").collection("review");
+    const paymentCollection = client.db("bistro-boss-DB").collection("payment");
 
-    app.get('/menu', async(req, res) => {
-        const result = await menuCollection.find().toArray();
-        res.send(result)
+
+    // verify middleware || 
+    const verifyToken = (req, res, next) => {
+
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      next();
+    };
+
+
+    // JWT releted API
+    app.post('/jwt', async (req, res) => {
+
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+      res.send({ token });
+    })
+
+
+
+    // Users releted API
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exits', insertedId: null });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     });
 
-    app.get('/review', async(req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result)
+
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+
     });
-  
+
+
+    // got ot support
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+
+      let admin = false;
+      if (user && user.role === 'admin') {
+        admin = true;
+      }
+
+      res.send({ admin });
+    });
+
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+
+      res.send(result)
+    });
+
+
+
+
+    //  menu Data Collection 
+
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result)
+    });
+
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result)
+    });
+
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+
+      res.send(result);
+    })
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+
+      res.send(result)
+    })
+
+    app.patch('/menu/:id', async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedItem = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image
+        }
+      }
+      const result = await menuCollection.updateOne(filter, updatedItem);
+      res.send(result)
+
+    });
+
+
+
+
+    // Carts releted API
+    app.post('/carts', async (req, res) => {
+
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+
+      res.send(result);
+    });
+
+    app.get('/carts', async (req, res) => {
+
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
+    // Payment intent
+    app.post('/creat-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.creat({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
+
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      };
+
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({ paymentResult, deleteResult })
+    })
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
- 
+
     // await client.close();
   }
 }
@@ -52,11 +278,21 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('No Problem...Server is Running')
+  res.send('No Problem...Server is Running')
 });
 
 app.listen(port, () => {
-    console.log(`Bistro Boss Server Running On Port:: ${port}`)
-})
+  console.log(`Bistro Boss Server Running On Port:: ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
 
 
